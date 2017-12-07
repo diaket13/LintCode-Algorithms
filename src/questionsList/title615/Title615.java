a85f4b09ca121c6fdf4e4e81d616f67a8aaaa8cc
yuxiwang1 <yuxiwang1@creditease.cn>
2017-12-07 17:52:44 +0800

    vxcvxc

---
+++ src/questionsList/title615/Title615.java
@@ -0,0 +1,10 @@
+package questionsList.title615;
+
+public class Title615 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
